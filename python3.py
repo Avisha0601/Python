63569f0f71c9ba1de15e61f9ffb6571ae31afdcd
Avisha0601 <86399682+Avisha0601@users.noreply.github.com>
2022-10-31 20:54:45 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,12 @@
+colours = ["white", "red", "grey", "green", "pink", "yellow", "blue", "cyan", "black", "purple"]
+print(colours)
+print(colours[0])
+print(colours[7])
+
+subjects = [["maths", "98%"],["english", "92%"],["science", "95%"],["geography", "99%"]]
+print(subjects)
+print(subjects[2])
+
+employees = [["James", "29", "P"],["lily", "25", "E"],["hermione", "30", "G"],["Draco","32", "M"],["Ron","29", "W"],["Lavender", "31", "B"]]
+print(employees)
+print(employees[3])
